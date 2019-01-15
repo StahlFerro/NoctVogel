3175b4fb3f42bc142f1c5c4d1cec1275c37c8139
StahlFerro <StahlFerro@users.noreply.github.com>
2019-01-15 16:44:11 +0700

    Added Discord.NET packages

--- Program.cs
+++ Program.cs
@@ -11,8 +11,10 @@ namespace NoctVogel
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting NoctVogel v0.0.1...");
             string jsonstring = File.ReadAllText("credentials.json");
             var cr = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonstring);
+            Console.WriteLine("Authenticating to Twitter...");
             Auth.SetUserCredentials(cr["consumerKey"], cr["consumerSecret"], cr["accessToken"], cr["accessSecret"]);
             var noct = User.GetAuthenticatedUser();
             var timeline = Timeline.GetHomeTimeline();
