e53b5726e7cfb357b09f654b8cebe3535e1b244c
StahlFerro <StahlFerro@users.noreply.github.com>
2019-01-15 13:08:38 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Tweetinvi;
+
+namespace NoctVogel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonstring = File.ReadAllText("credentials.json");
+            var cr = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonstring);
+            Auth.SetUserCredentials(cr["consumerKey"], cr["consumerSecret"], cr["accessToken"], cr["accessSecret"]);
+            var noct = User.GetAuthenticatedUser();
+            var timeline = Timeline.GetHomeTimeline();
+            var creator = User.GetUserFromScreenName("StahlFerroMLG");
+            Console.WriteLine(noct.ScreenName);
+            Console.WriteLine(creator.ScreenName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# NoctVogel
+
+A twitter bot using the [Tweetinvi](https://github.com/linvi/tweetinvi) library
