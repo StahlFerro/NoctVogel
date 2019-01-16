c6ae15f07d6e2cca2aaa0ecd64c7463c3a50aa6b
StahlFerro <StahlFerro@users.noreply.github.com>
2019-01-17 02:54:13 +0700

    Added prototype twitter user information lookup command

--- Modules/TwitterCommands.cs
+++ Modules/TwitterCommands.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Discord;
@@ -8,6 +9,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Tweetinvi;
 using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace NoctVogel.Modules
 {
@@ -30,6 +32,38 @@ namespace NoctVogel.Modules
                 await ReplyAsync("Tweet message cannot be more than 140 letters");
             tweetuser.PublishTweet(text);
             await ReplyAsync("Tweet successfully sent");
+        }
+
+        [Command("userinfo", RunMode = RunMode.Async)]
+        [Alias("uinfo")]
+        [RequireOwner]
+        public async Task UserInfo([Remainder] string username)
+        {
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+            var user = User.GetUserFromScreenName(username);
+            if (user == null)
+                await ReplyAsync($"User with username {username} not found");
+            var timelineparam = new UserTimelineParameters()
+            {
+                MaximumNumberOfTweetsToRetrieve = int.MaxValue,
+                IncludeRTS = true,
+                IncludeEntities = true,
+                IncludeContributorDetails = true
+            };
+            long tweetcount = (await user.GetUserTimelineAsync(1000)).Count();
+            long followercount = user.FollowersCount;
+            long followingcount = (await user.GetFriendIdsAsync(int.MaxValue -1)).Count();
+            long likecount = user.FavouritesCount;
+            long listcount = user.ListedCount;
+            var statistics = $"Tweets: {tweetcount}\nFollowing: {followingcount}\nFollowers: {followercount}\n" +
+                $"Likes: {likecount}\nLists: {listcount}";
+            var E = new EmbedBuilder()
+                .WithTitle($"{user.Name} @{user.ScreenName}")
+                .WithThumbnailUrl(user.ProfileImageUrl)
+                .WithDescription(user.Description)
+                .AddField("Statistics", statistics, true)
+                .AddField("Identifier", user.UserIdentifier, true);
+            await ReplyAsync("", embed: E.Build());
         }
     }
 }
